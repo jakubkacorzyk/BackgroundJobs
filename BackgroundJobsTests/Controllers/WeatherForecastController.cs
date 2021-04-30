ab05d7d8713815b0b7f4e50f34987cb1a2aa6897
Jakub Kacorzyk <jakub.kacorzyk@polpc08245.allegrogroup.internal>
2021-04-30 14:14:04 +0200

    Hangfire tests

--- BackgroundJobsTests/Controllers/WeatherForecastController.cs
+++ BackgroundJobsTests/Controllers/WeatherForecastController.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
+using Hangfire;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 
@@ -15,6 +17,9 @@ namespace BackgroundJobsTests.Controllers
         {
             "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
+
+        private static bool _isWeatherJobDone;
+        private static int _numberOfFails = 0;
 
         private readonly ILogger<WeatherForecastController> _logger;
 
@@ -34,6 +39,71 @@ namespace BackgroundJobsTests.Controllers
                     Summary = Summaries[rng.Next(Summaries.Length)]
                 })
                 .ToArray();
+        }
+        
+        [HttpGet("/jobTest")]
+        public void BackgroundJobTest()
+        {
+            BackgroundJob.Enqueue(() => GetWeather());
+        }
+
+        [HttpGet("/jobTest/fails/{numberOfFailures}")]
+        public void BackgroundJob3xFailedTest(int numberOfFailures)
+        {
+            _numberOfFails = 0;
+            BackgroundJob.Enqueue(() => GetWeatherFailures(numberOfFailures));
+        }
+
+        [HttpGet("/jobTest/check")]
+        public bool CheckJob()
+        {
+            return _isWeatherJobDone;
+        }
+
+        [HttpGet("/continuationTest")]
+        public void ContinuationJobTest()
+        {
+            var jobId = BackgroundJob.Schedule(
+                () => Console.WriteLine("Delayed!"),
+                TimeSpan.FromSeconds(10));
+            BackgroundJob.ContinueJobWith(
+                jobId,
+                () => GetWeather());
+        }
+
+        public static void GetWeather()
+        {
+            _isWeatherJobDone = false;
+            var rng = new Random();
+            Enumerable.Range(1, 5).Select(index => new WeatherForecast
+                {
+                    Date = DateTime.Now.AddDays(index),
+                    TemperatureC = rng.Next(-20, 55),
+                    Summary = Summaries[rng.Next(Summaries.Length)]
+                })
+                .ToArray();
+            Thread.Sleep(10000);
+            _isWeatherJobDone = true;
+        }
+        
+        public static void GetWeatherFailures(int fails)
+        {
+            _isWeatherJobDone = false;
+            var rng = new Random();
+            Enumerable.Range(1, 5).Select(index => new WeatherForecast
+                {
+                    Date = DateTime.Now.AddDays(index),
+                    TemperatureC = rng.Next(-20, 55),
+                    Summary = Summaries[rng.Next(Summaries.Length)]
+                })
+                .ToArray();
+            Thread.Sleep(10000);
+            if (_numberOfFails <= fails)
+            {
+                _numberOfFails++;
+                throw new Exception($"Failure number {_numberOfFails}/{fails}");
+            }
+            _isWeatherJobDone = true;
         }
     }
 }
--- BackgroundJobsTests/Startup.cs
+++ BackgroundJobsTests/Startup.cs
@@ -1,15 +1,11 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Hangfire;
+using Hangfire.PostgreSql;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
 
 namespace BackgroundJobsTests
@@ -31,10 +27,16 @@ namespace BackgroundJobsTests
             {
                 c.SwaggerDoc("v1", new OpenApiInfo {Title = "BackgroundJobsTests", Version = "v1"});
             });
+            services.AddHangfire(configuration => configuration
+                .SetDataCompatibilityLevel(CompatibilityLevel.Version_170)
+                .UseSimpleAssemblyNameTypeSerializer()
+                .UseRecommendedSerializerSettings()
+                .UsePostgreSqlStorage(Configuration.GetConnectionString("HangfireConnection")));
+            services.AddHangfireServer();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IBackgroundJobClient backgroundJobs, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
@@ -49,7 +51,11 @@ namespace BackgroundJobsTests
 
             app.UseAuthorization();
 
-            app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+                endpoints.MapHangfireDashboard();
+            });
         }
     }
 }
